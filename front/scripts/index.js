5434c909c7acfe3e955ec98e6c6defd62e7efeb0
pablo putin <pablopunti@gmail.com>
2025-05-21 19:38:11 -0300

    primer y segundo trabajo incompleto

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,19 @@
-console.log(tempData);
+const contenedor = document.getElementById("peliculas");
+
+tempData.forEach(pelicula =>{
+
+    const card = `
+    <div class="col-xl-4 col-md-6 col-xs-10">
+    <h6 class="titulo">${pelicula.title}</h6>
+    <img src="${pelicula.poster}" alt="${pelicula.title}">}
+      <p class="card-text">
+            <strong>Año:</strong> ${pelicula.year}<br>
+            <strong>Director:</strong> ${pelicula.director}<br>
+            <strong>Duración:</strong> ${pelicula.duration}<br>
+            <strong>Géneros:</strong> ${pelicula.genre.join(", ")}<br>
+            <strong>Calificación:</strong> ${pelicula.rate}
+          </p> 
+    </div>
+    `;
+    contenedor.innerHTML += card;
+});
